dd5ae42be60b484adadb25935885ebbf2bc8f387
ZQZ44 <zqz235711@gmail.com>
2021-08-01 22:36:48 +0800

    week2 update

--- week2/warperror.go
+++ week2/warperror.go
@@ -24,30 +24,3 @@ func dao() err {
         return errors.Warp(err, "dao sql ErrNoRow")
     }
 }
-import (
-        "database/sql"
-        "os"
-        "fmt"
-        "github.com/pkg/errors"
-)
-
-/*  在 dao 层遇到sql.ErrNoRows，应该调用Warp 及时定位错误并保存堆栈信息，然而返回给上层，防止无法定位错误发生点的麻烦。
-*/
-
-func main() {
-    err := dao()
-    if err != nil {
-        fmt.Printf("Error: %+v\n", err)
-        os.Exit(1)
-    }
-}
-
-func dao() err {
-    db, err := sql.open("......",".....")
-    var sth string
-    err = db.QueryRow("......").Scan(&sth)
-    if err == sql.ErrNoRows {
-        return errors.Warp(err, "dao sql ErrNoRow")
-    }
-}
-
